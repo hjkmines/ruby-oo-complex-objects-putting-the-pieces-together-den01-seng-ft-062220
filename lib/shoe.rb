08fcbebc3afd5f09d64dc5be3ea4bc2993c9e5ac
Hyung Kim <hjkmines@gmail.com>
2020-06-25 02:06:52 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,16 @@
 # Make your shoe class here!
 class Shoe 
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand = brand
+  end 
+  
+  def cobble 
+    puts "shoe has been repaired"
+    @condition = new 
+  end
+  
   
   
   
