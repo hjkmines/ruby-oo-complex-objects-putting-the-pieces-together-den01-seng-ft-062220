e5d22bc0695fdb5ae3f85628a69faef046280c95
Hyung Kim <hjkmines@gmail.com>
2020-06-25 02:00:46 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,6 +3,26 @@ class Book
    @title = title 
   end 
   
+  def title 
+    @title 
+  end 
+  
+  def author=(author)
+    @author = author 
+  end 
+  
+  def author 
+    @author 
+  end 
+  
+  def page_count=(num)
+    @page_count = num 
+  end 
+  
+  def page_count
+    @page_count
+  end
+    
   
 end 
 
